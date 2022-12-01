d78a0a8b47ab8d21f67f42c867948c870857fc59
Bob Desaunois <bobdesaunosi@gmail.com>
2022-12-01 13:27:40 +0100

    Removed unneccesary imports

--- DayOne/ElfCalorieCounter.cs
+++ DayOne/ElfCalorieCounter.cs
@@ -1,10 +1,5 @@
 ﻿using DayOne.Entities;
 using DayOne.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DayOne
 {
--- DayOne/Repositories/CaloriesRepository.cs
+++ DayOne/Repositories/CaloriesRepository.cs
@@ -1,11 +1,4 @@
 ﻿using DayOne.Entities;
-using Microsoft.Win32.SafeHandles;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DayOne.Repositories
 {
